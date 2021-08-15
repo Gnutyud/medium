10c1db06b0958944aecddf2dbf0051c8a054e7ee
Dat <datlthe130450@fpt.edu.vn>
2021-08-15 19:44:24 +0700

    add slice new Article

--- src/features/articles/slice/articleSlice.tsx
+++ src/features/articles/slice/articleSlice.tsx
@@ -1,40 +1,62 @@
-import {RootState} from '../../../app/store';
-import {ArticleType} from '../helpers/articleType';
-import {fetchArticles} from '../api/articlesAPI';
-import {createAsyncThunk, createSlice, nanoid, PayloadAction} from '@reduxjs/toolkit';
-
+import { RootState } from "../../../app/store";
+import { ArticleType } from "../helpers/articleType";
+import { fetchArticles, postArticle } from "../api/articlesAPI";
+import {
+  createAsyncThunk,
+  createSlice,
+  nanoid,
+  PayloadAction,
+} from "@reduxjs/toolkit";
 
 export interface ArticleState {
   articles: ArticleType[];
 }
 
 const initialState: ArticleState = {
-  articles: []
+  articles: [],
 };
 
 export const articlesAsync = createAsyncThunk(
-  'articles/fetchArticles',
+  "articles/fetchArticles",
   async () => {
     const response = await fetchArticles();
     return response.articles;
   }
 );
 
+export const createArticle = createAsyncThunk(
+  "articles/postArticles",
+  async (data) => {
+    const response = await postArticle(data);
+    return response.articles;
+  }
+);
+
 export const articlesSlice = createSlice({
-  name: 'articles',
+  name: "articles",
   initialState,
   reducers: {},
-  extraReducers: builder => {
-    builder.addCase(articlesAsync.fulfilled, (state, action: PayloadAction<ArticleType[]>) => {
-      state.articles = action.payload.map(article => {
-        const randomId = nanoid();
-        return {
-          ...article,
-          id: randomId
-        };
-      });
-    });
-  }
+  extraReducers: (builder) => {
+    builder
+      .addCase(
+        articlesAsync.fulfilled,
+        (state, action: PayloadAction<ArticleType[]>) => {
+          state.articles = action.payload.map((article) => {
+            const randomId = nanoid();
+            return {
+              ...article,
+              id: randomId,
+            };
+          });
+        }
+      )
+      .addCase(
+        createArticle.fulfilled,
+        (state, action: PayloadAction<ArticleType>) => {
+          state.articles.push(action.payload);
+        }
+      );
+  },
 });
 
 // selector
