efc02754eb257dcbbad6b401a8ad38f1bee2e3dc
Dat <datlthe130450@fpt.edu.vn>
2021-08-30 19:57:17 +0700

    setup interceptors token

--- src/api/articlesApi.ts
+++ src/api/articlesApi.ts
@@ -1,8 +1,5 @@
 import axiosClient from './axiosClient';
 
-const Storage: any = localStorage.getItem('user');
-const user = JSON.parse(Storage);
-
 const articlesApi = {
   getAll: (
     offsetParam?: number,
@@ -10,57 +7,24 @@ const articlesApi = {
     tagParam?: string,
     authorNameParam?: string
   ): Promise<ArticleType[]> => {
-    const axiosConfig = user
-      ? {
-          headers: {
-            Authorization: 'Bearer ' + user.token,
-          },
-          params: {
-            offset: offsetParam,
-            limit: limitParam,
-            tag: tagParam,
-            author: authorNameParam,
-          },
-        }
-      : {
-          params: {
-            offset: offsetParam,
-            limit: limitParam,
-            tag: tagParam,
-            author: authorNameParam,
-          },
-        };
+    const axiosConfig = {
+      params: {
+        offset: offsetParam,
+        limit: limitParam,
+        tag: tagParam,
+        author: authorNameParam,
+      },
+    };
     return axiosClient.get('/articles', axiosConfig);
   },
   addOne: (data: { article: FormInputArticleType }): Promise<FormInputArticleType> => {
-    let Storage: any = localStorage.getItem('user');
-    let user = JSON.parse(Storage);
-    let axiosConfig = {
-      headers: {
-        Authorization: 'Bearer ' + user.token,
-      },
-    };
-    return axiosClient.post('/articles', data, axiosConfig);
+    return axiosClient.post('/articles', data);
   },
   favoriteArticle: (slug: string): Promise<ArticleType> => {
-    let Storage: any = localStorage.getItem('user');
-    let user = JSON.parse(Storage);
-    let axiosConfig = {
-      headers: {
-        Authorization: 'Bearer ' + user.token,
-      },
-    };
-    return axiosClient.post(`/articles/${slug}/favorite`, null, axiosConfig);
+    return axiosClient.post(`/articles/${slug}/favorite`);
   },
   unfavoriteArticle: (slug: string): Promise<ArticleType> => {
-    let Storage: any = localStorage.getItem('user');
-    let user = JSON.parse(Storage);
-    let axiosConfig = {
-      headers: {
-        Authorization: 'Bearer ' + user.token,
-      },
-    };
-    return axiosClient.delete(`/articles/${slug}/favorite`, axiosConfig);
+    return axiosClient.delete(`/articles/${slug}/favorite`);
   },
 };
 
