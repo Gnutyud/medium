f35da0af4cc48ed56fb83af28f453dec8e8c99be
Nguyễn Duy Tùng <73052334+Gnutyud@users.noreply.github.com>
2021-09-01 10:16:39 +0700

    added shadow article

--- src/components/common/Article.tsx
+++ src/components/common/Article.tsx
@@ -38,6 +38,11 @@ const useStyles = makeStyles((theme) =>
       justifyContent: 'space-between',
       marginRight: '30px',
       width: '90%',
+      boxShadow: '0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24)',
+      transition: 'all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1)',
+      '&:hover': {
+        boxShadow: '0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22)',
+      },
       [theme.breakpoints.down('sm')]: {
         width: '100%',
         margin: '0',
