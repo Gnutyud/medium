8c0283fdf63b3415d5129976d8370758eeee2de2
datpham-123 <datpham123335@gmail.com>
2021-09-01 12:12:23 +0700

    update loading animation

--- src/components/common/Loading.tsx
+++ src/components/common/Loading.tsx
@@ -3,7 +3,7 @@ import Loader from 'react-loader-spinner';
 
 const Loading = () => {
   return (
-    <Box>
+    <Box style={{ textAlign: 'center' }}>
       <Loader type="Circles" color="#00BFFF" height={80} width={80} />
     </Box>
   );
