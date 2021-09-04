347ba14ff28805b263964348052f7ef889beeaa3
Dat <datlthe130450@fpt.edu.vn>
2021-09-04 21:17:41 +0700

    add test folder

---
+++ src/features/auth/__tests__/LoginForm.test.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+
+describe('<LoginPage />', () => {
+  test('should display a blank login form', async () => {
+    // do something
+  });
+  it('button sign up', () => {});
+});
