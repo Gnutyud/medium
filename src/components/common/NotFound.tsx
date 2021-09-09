b522b76cba238a06741372a2013a1b83635906d4
datpham-123 <datpham123335@gmail.com>
2021-09-09 19:42:34 +0700

    fix profile not found

--- src/components/common/NotFound.tsx
+++ src/components/common/NotFound.tsx
@@ -3,7 +3,16 @@ import React from 'react';
 
 export const NotFound = () => {
   return (
-    <Typography style={{ textAlign: 'center' }} variant="h3">
+    <Typography
+      style={{
+        textAlign: 'center',
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+      variant="h3"
+    >
       Not Found!!
     </Typography>
   );
--- src/features/profile/profileSlice.ts
+++ src/features/profile/profileSlice.ts
@@ -4,11 +4,13 @@ import { createSlice } from '@reduxjs/toolkit';
 interface InitialState {
   profile: ProfileType;
   isLoading: boolean;
+  error?:any;
 }
 
 const initialState: InitialState = {
   profile: {} as ProfileType,
   isLoading: false,
+  error:null,
 };
 
 export const authorSlice = createSlice({
@@ -20,14 +22,21 @@ export const authorSlice = createSlice({
     },
     getProfileSuccess: (state, action) => {
       state.profile = action.payload.profile;
+      state.error = null;
     },
     followProfile: (state) => {},
     followProfileSuccess: (state, action) => {
       state.profile = action.payload.profile;
+      state.error = null;
     },
     unFollowProfile: (state) => {},
     unFollowProfileSuccess: (state, action) => {
       state.profile = action.payload.profile;
+      state.error = null;
+    },
+    getError: (state, action) => {
+      state.isLoading = false;
+      state.error = action.payload;
     },
   },
 });
@@ -45,5 +54,6 @@ export const {
 // selector
 export const selectProfile = (state: RootState) => state.profile.profile;
 export const selectIsLoading = (state: RootState) => state.profile.isLoading;
+export const selectError = (state: RootState) => state.oneArticleReducer.error;
 
 export default authorSlice.reducer;
